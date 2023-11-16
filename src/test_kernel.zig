b91a31f2b842f5bdad0b1e308f0d4c8622f7766b
Robin Voetter <robin@voetter.nl>
2023-11-16 20:30:23 +0100

    restore test kernel

--- src/test_kernel.zig
+++ src/test_kernel.zig
@@ -1,18 +1,11 @@
 const std = @import("std");
-const mem = std.mem;
-const expect = std.testing.expect;
-const expectEqual = std.testing.expectEqual;
-const assert = std.debug.assert;
 
-test "peer type resolution: error union and optional of same type" {
-    const E = error{Foo};
-    var a: E!*u8 = error.Foo;
-    var b: ?*u8 = null;
+test "basic" {}
 
-    var t = true;
-    const r1 = if (t) a else b;
+test "fail" {
+    return error.Fail;
+}
 
-    const T = @TypeOf(r1);
-
-    try expectEqual(@as(T, error.Foo), r1);
+test "skip" {
+    return error.SkipZigTest;
 }
