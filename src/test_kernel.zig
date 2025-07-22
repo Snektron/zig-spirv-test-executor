57fa03b5cffe46368a7e7eafc5fd404a2563f2b3
Ivan Stepanov <ivanstepanovftw@gmail.com>
2025-07-22 10:05:15 +0200

    skip for non-SPIRV targets

--- src/test_kernel.zig
+++ src/test_kernel.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 test "basic" {}
 
@@ -7,6 +8,7 @@ test "skip" {
 }
 
 test "workgroup builtins" {
+    if (builtin.zig_backend != .stage2_spirv) return error.SkipZigTest;
     try std.testing.expectEqual(0, @workGroupId(0));
     try std.testing.expectEqual(1, @workGroupSize(0));
     try std.testing.expectEqual(0, @workItemId(0));
